69cb52e2ae76ce2dfacdd9dfbd47d00195704c91
Tommy Takahashi <ttakahashi1591@gmail.com>
2024-01-14 13:49:31 -0700

    feat: Add dream driving for ForecastFacade and ForecastSerializer

--- app/controllers/api/v0/forecast_controller.rb
+++ app/controllers/api/v0/forecast_controller.rb
@@ -1,5 +1,7 @@
 class Api::V0::ForecastController < ApplicationController
   def index
-    
+    forecast = ForecastFacade.city_search(params[:location])
+
+    render json: ForecastSerializer.new(forecast).as_json
   end
 end
