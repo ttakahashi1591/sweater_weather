66c2c199b19c566d085fb63289fb3c1e5303884b
Tommy Takahashi <ttakahashi1591@gmail.com>
2024-01-14 14:43:04 -0700

    Add webmock and vcr gems as well as setup for each to start utilzing for testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -72,3 +72,11 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.filter_sensitive_data('<GEOCODING_KEY>') { Rails.application.credentials.geocoding[:key] }
+  config.configure_rspec_metadata!
+  config.default_cassette_options = { re_record_interval: 7.days, record: :new_episodes }
+end
