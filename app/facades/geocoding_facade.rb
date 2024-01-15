5335c1e4cd09b9acfc970110d55fe9670886abbd
Tommy Takahashi <ttakahashi1591@gmail.com>
2024-01-14 18:11:27 -0700

    roadblock: Having issues with faraday connection to Mapquest API

--- app/facades/geocoding_facade.rb
+++ app/facades/geocoding_facade.rb
@@ -1,7 +1,7 @@
 class GeocodingFacade 
   def coordinates(location)
     response = service.location_data(location)
-
+    
     JSON.parse(response.body, symbolize_names: true)[:results][0][:locations][0][:displayLatLng]
   end
 
